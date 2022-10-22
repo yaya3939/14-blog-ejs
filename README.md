ef270923f8b2fb33ccea3118ab83a5e40539e9bf
yaya3939 <1220593514@qq.com>
2022-10-22 13:28:07 +0800

    complete

---
+++ README.md
@@ -0,0 +1,32 @@
+## 学了啥
+
+- 10/21
+- 有问题一定要先看文档，找相关的 tag 的信息。ejs 里的 include，要怎么使用 views 子文件夹里的文件
+- array.forEach(function(element) {})
+- nodemon 只有在 js 文件做改动的时候才会自动重启
+- express 的 route parameters
+- array 里是 number/sring 时的元素查询方法：indexOf()，includes();
+
+array 里是 object 时，查询各个 object 里的元素的方法：some();
+
+- 10/22
+- 使用 lodash 解决 url 的大小写和—问题
+- css 和 js 的不同的显示有限字符的方式
+
+css：display,white-space,width,text-overflow...
+
+js：substring()
+
+- 怎么把 title 转变为 url 形式：.toLowerCase().replace(" ","-")
+
+join():一个 array 的 method，所以我一直用在 string 上就一直出错
+
+虽然最后其实没有用上。因为在 app.js 的"posts..."那个 route 里，不管你 type 的 title 是什么样式的，在 function 里其实已经通过 lodash 格式化了，所以不需要在意 type title 的形式，而我完全没有注意到，所以花了很多时间在 ejs 里 format route
+
+- 为了更好地理解代码逻辑，一定要给变量起最准确最贴合逻辑的名字，并且不要有重复
+
+## 问题
+
+- √：header.ejs&footer.ejs 里的标签老是自动补足，让我的 body 外观出了点问题。 sulotion: disable format on save
+- footer in compose page
+- 用 foreach 做一次 matched
---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//datas
+const homeStartingConten =
+  "Lorem ipsum dolor sit amet consectetur adipiscing elit enim, diam iaculis vitae turpis dis aliquam ex scelerisque fames, cubilia habitant magna penatibus feugiat fusce dictumst. Porttitor blandit lacinia cras neque habitasse ex egestas suscipit, platea class sollicitudin magnis duis auctor tempus, ipsum fames laoreet eget cursus litora hac. Orci elementum mi ut arcu nec nulla habitasse, potenti hac duis curae venenatis convallis, primis faucibus semper massa scelerisque dictum. Ligula scelerisque maecenas habitasse risus dignissim platea pretium velit feugiat ullamcorper, non nunc blandit dui arcu ridiculus conubia tortor tempus vel ut, cubilia quam suscipit vestibulum est justo dictum mattis mus. Iaculis et volutpat lectus nascetur arcu tempor maximus accumsan ridiculus, leo porttitor etiam viverra conubia est eros turpis, efficitur ex imperdiet fermentum feugiat pretium mi suspendisse.";
+const aboutStartingConten =
+  "Orci elementum mi ut arcu nec nulla habitasse, potenti hac duis curae venenatis convallis, primis faucibus semper massa scelerisque dictum. Ligula scelerisque maecenas habitasse risus dignissim platea pretium velit feugiat ullamcorper, non nunc blandit dui arcu ridiculus conubia tortor tempus vel ut, cubilia quam suscipit vestibulum est justo dictum mattis mus. Iaculis et volutpat lectus nascetur arcu tempor maximus accumsan ridiculus, leo porttitor etiam.";
+const contactContent =
+  "Yeros est litora commodo magna nec etiam vulputate, senectus turpis nam habitant duis nibh. Molestie cursus lectus risus aenean est dapibus ultricies sollicitudin dui netus, amet hac tempus tincidunt placerat luctus senectus a conubia, purus commodo suscipit elementum accumsan eros quis non maximus.";
+
+var posts = [];
+
+//gets
+app.get("/", function (req, res) {
+  // const
+  // const readTitle = posts.post.title.toLowerCase().join("-");
+  // console.log(readTitle);
+
+  res.render("home", {
+    startingContent: homeStartingConten,
+    yourPosts: posts,
+    // readAnchor:
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { startingContent: aboutStartingConten });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { startingContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.get("/posts/:typtTitle", function (req, res) {
+  const urlTtile = _.lowerCase(req.params.typeTitle);
+  //format了type进去的title，所以不用在意type title的样式
+
+  //当array里是object的时候，怎么查询object里的元素存在不存在
+  //   const result= posts.some(function (arrVal) {
+  //    return urlTtile = arrVal;
+  //  })
+
+  //the way angela used
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    //不要忘记const的scope，所以要在这个函数内使用它
+
+    if (storedTitle === urlTtile) {
+      res.render("post", { postTitle: post.title, postContent: post.content });
+    }
+  });
+  //因为涉及到大小写变换和—，所以不能直接用array的元素检索
+});
+
+//post
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.title,
+    content: req.body.postContent,
+  };
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000.");
+});
